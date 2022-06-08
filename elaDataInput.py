4e47dfdd80b067c6096b5786dea30429d381b39c
shinhoghim <shinhoghim@gmail.com>
2022-06-08 13:37:31 +0900

    .

--- elaDataInput.py
+++ elaDataInput.py
@@ -1,26 +1,31 @@
-from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 import json
 
-es = Elasticsearch("http://192.168.5.133:9200")
+# es = Elasticsearch("http://192.168.5.133:9200")
 
 
 n=0
-file = open('ex1.json','r',encoding='utf-8')
+file = open('qnaSample.json','r',encoding='utf-8')
 for line in file.readlines():
 	dic = json.loads(line)
 	doc = {
-		"public_date":dic["public_date"],
-		"newspaper":dic["newspaper"],
-		"title":dic["title"],
-		"content":dic["content"]
+		"title": dic["title"],
+		"url" : dic["url"],
+		"date" : dic["date"],
+		"question" : dic["question"],
+		"answer" : dic["answer"]
+		# "public_date":dic["public_date"],
+		# "newspaper":dic["newspaper"],
+		# "title":dic["title"],
+		# "content":dic["content"]
 		}
 	
-	#print(doc)
+	print(dic, "\n")
 
-	res = es.index(index="kogundata", doc_type="_doc", id=n+1, body=doc)
+	# res = es.index(index="kogundata", doc_type="_doc", id=n+1, body=doc)
 	n=n+1
 	#print(n)
 	#print(res)
 	#print(dic)
 	#print(dic["title"])
-print("data count: _" ,n)
+print("data count: _" ,n,"\n")
