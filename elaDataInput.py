5dbc13639a5feecb6030df8ff6a412c346bde0cb
shinhoghim <shinhoghim@gmail.com>
2022-06-08 16:05:27 +0900

    .

--- elaDataInput.py
+++ elaDataInput.py
@@ -3,22 +3,21 @@ import json
 
 # es = Elasticsearch("http://192.168.5.133:9200")
 
-
 n=0
-file = open('qnaSample.json','r',encoding='utf-8')
+file = open('./qnaSample.json','r',encoding='UTF-8')
 for line in file.readlines():
 	dic = json.loads(line)
-	doc = {
-		"title": dic["title"],
-		"url" : dic["url"],
-		"date" : dic["date"],
-		"question" : dic["question"],
-		"answer" : dic["answer"]
-		# "public_date":dic["public_date"],
-		# "newspaper":dic["newspaper"],
-		# "title":dic["title"],
-		# "content":dic["content"]
-		}
+	# doc = {
+	# 	"title": dic["title"],
+	# 	"url" : dic["url"],
+	# 	"date" : dic["date"],
+	# 	"question" : dic["question"],
+	# 	"answer" : dic["answer"]
+	# 	# "public_date":dic["public_date"],
+	# 	# "newspaper":dic["newspaper"],
+	# 	# "title":dic["title"],
+	# 	# "content":dic["content"]
+	# 	}
 	
 	print(dic, "\n")
 
--- jsonFileRead.py
+++ jsonFileRead.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-
-with open("./saveJsonTest.json",'r', encoding='UTF8') as f:
+file = open('./qnaSample.json','r',encoding='UTF-8')
+with open("./qnaSample.json",'r', encoding='UTF8') as f:
   data = json.load(f,strict=False)
 
 print(data)
